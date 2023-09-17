8adfc8591673dbf32b31aa1e9d1e2998ca1eac8a
Aamer <mdaamer456@gmail.com>
2023-09-17 19:57:58 +0530

    Contact US page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Navbar from './components/Navigation';
 import Hero from './components/Hero'
+import Contactus from "./components/Contactus";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App w-full h-screen overflow-hidden">
       <Navbar />
       <Hero />
+      {/* <Contactus /> */}
     </div>
   );
 }
